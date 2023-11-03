07cedbb3d3bf66c157d718e87f490500fc86d643
santiemanuel <santiemanuel@gmail.com>
2023-11-02 21:01:14 -0300

    Modelos y relaciones

--- learning/admin.py
+++ learning/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Curso
 
 # Register your models here.
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    fields = ["nombre", "descripcion", ("fecha_publicacion", "precio")]
+    list_display = ("nombre", "descripcion", "precio", "fecha_publicacion")
+    search_fields = ("nombre", "descripcion")
+    list_filter = ("precio", "fecha_publicacion")
+    ordering = ("nombre", "-precio")
