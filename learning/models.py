33d817e5f0e061ee52db878242bbd417b9db2244
santiemanuel <santiemanuel@gmail.com>
2023-12-05 12:12:48 -0300

    Eliminando managers

--- learning/models.py
+++ learning/models.py
@@ -4,34 +4,6 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.text import slugify
 
 
-class FreeCoursesManager(models.Manager):
-    """Manager para obtener todos los cursos gratis."""
-
-    def get_queryset(self):
-        return super().get_queryset().filter(precio=0)
-
-
-class PremiumCoursesManager(models.Manager):
-    """Manager para obtener todos los cursos premium."""
-
-    def get_queryset(self):
-        return super().get_queryset().filter(precio__gt=600)
-
-
-class CursoByYearManager(models.Manager):
-    """Manager para obtener los cursos por año."""
-
-    def courses_in_year(self, year):
-        return self.filter(fecha_publicacion__year=year)
-
-
-class CursoByTopicManager(models.Manager):
-    """Manager para obtener los cursos por temática."""
-
-    def courses_for_topic(self, topic_name):
-        # Este es un ejemplo simple. En la realidad, deberíamos crear un campo o modelo independiente para los temas.
-        return self.filter(nombre__icontains=topic_name)
-
 class CustomUser(AbstractUser):
     ESTUDIANTE = 'estudiante'
     INSTRUCTOR = 'instructor'
@@ -42,6 +14,7 @@ class CustomUser(AbstractUser):
     ]
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=ESTUDIANTE)
+
 
 class Instructor(models.Model):
     usuario = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
@@ -66,7 +39,6 @@ class Estudiante(models.Model):
         return f"{self.nombre} ({self.email})"
 
 
-# Create your models here.
 class Curso(models.Model):
     nombre = models.CharField(max_length=120)
     descripcion = models.TextField()
@@ -77,11 +49,6 @@ class Curso(models.Model):
     is_deleted = models.BooleanField(default=False)
 
     slug = models.SlugField(max_length=120, unique=True, null=True, blank=True)
-
-    # free_courses = FreeCoursesManager()
-    # premium_courses = PremiumCoursesManager()
-    # by_year = CursoByYearManager()
-    # by_topic = CursoByTopicManager()
 
     def save(self, *args, **kwargs):
         if not self.slug:
