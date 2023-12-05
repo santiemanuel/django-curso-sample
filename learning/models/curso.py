a3cd7705ae34156debed60308483c069aa597f11
santiemanuel <santiemanuel@gmail.com>
2023-12-05 15:10:40 -0300

    Update modelo de curso

--- learning/models/curso.py
+++ learning/models/curso.py
@@ -5,15 +5,30 @@ from django.utils.text import slugify
 
 
 class Curso(models.Model):
+
+    # Página 1: Información General del Curso
     nombre = models.CharField(max_length=120)
     descripcion = models.TextField()
-    precio = models.IntegerField()  # $250 $250.00, $262.97 => 262.97
+    precio = models.IntegerField()
+
+    # Página 2: Detalles y Metodología del Curso
+    METODOLOGIA_CHOICES = [
+        ('online', 'Online'),
+        ('presencial', 'Presencial'),
+        ('semipresencial', 'Semipresencial'),
+    ]
+    metodologia = models.CharField(max_length=15, choices=METODOLOGIA_CHOICES, default='online', verbose_name="Metodología del Curso")
+    objetivos = models.TextField(verbose_name="Objetivos del Curso", null=True, blank=True)
+    contenido = models.TextField(verbose_name="Contenido del Curso", null=True, blank=True)
+
+    # Página 3: Información Complementaria
     fecha_publicacion = models.DateField()
     instructor = models.ForeignKey(Instructor, on_delete=models.SET_NULL, null=True, blank=True)
-    inscripciones = models.ManyToManyField(Estudiante, through="Inscripcion")
-    is_deleted = models.BooleanField(default=False)
 
+    # Datos internos
+    inscripciones = models.ManyToManyField(Estudiante, through="Inscripcion")
     slug = models.SlugField(max_length=120, unique=True, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         if not self.slug:
